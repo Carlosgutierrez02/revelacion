39af1005029582ff33cdd26c99a389b4e4c5c063
Carlosgutierrez02 <116392517+Carlosgutierrez02@users.noreply.github.com>
2026-04-28 14:04:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function entrar(){
+    document.getElementById("intro").classList.add("hidden");
+    setTimeout(()=>{
+        document.getElementById("main").classList.remove("hidden");
+    },500);
+}
+
+/* WHATSAPP */
+function confirmar(){
+    const mensaje = `Hola 👋✨
+
+Confirmo mi asistencia a la revelación de género 🩷💙
+
+📅 14 de junio de 2026
+⏰ 3:00 PM
+
+Será un gusto acompañarlos 🤍`;
+
+    const url = "https://api.whatsapp.com/send?phone=573213971526&text=" + encodeURIComponent(mensaje);
+    window.open(url, "_blank");
+}
+
+/* ANIMACIÓN TETEROS */
+const bg = document.getElementById("cardBg");
+
+function crearTetero(){
+
+    const div = document.createElement("div");
+    div.classList.add("icon");
+
+    const img = document.createElement("img");
+    img.src = "img/biberon.png";
+
+    div.appendChild(img);
+
+    const size = 20 + Math.random()*30;
+    div.style.width = size + "px";
+
+    div.style.left = Math.random()*bg.offsetWidth + "px";
+
+    let y = -60;
+    let rot = Math.random()*360;
+
+    const speed = 1 + Math.random()*2;
+    const rotSpeed = (Math.random()-0.5)*2;
+
+    div.style.opacity = 0.2 + Math.random()*0.4;
+
+    bg.appendChild(div);
+
+    function animar(){
+        y += speed;
+        rot += rotSpeed;
+
+        div.style.transform = `translateY(${y}px) rotate(${rot}deg)`;
+
+        if(y < bg.offsetHeight + 80){
+            requestAnimationFrame(animar);
+        }else{
+            div.remove();
+        }
+    }
+
+    requestAnimationFrame(animar);
+}
+
+setInterval(crearTetero,700);
